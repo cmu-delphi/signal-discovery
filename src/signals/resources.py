d691729635c466d9f7a35aff37df0394381472e8
Dmytro Trotsko <dmytrotsko@gmail.com>
2024-11-04 21:24:40 +0200

    Added error rows import handling

--- src/signals/resources.py
+++ src/signals/resources.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from import_export import resources
 from import_export.fields import Field, widgets
+from import_export.results import RowResult
 
 from datasources.models import SourceSubdivision
 from datasources.resources import process_links
@@ -152,7 +153,35 @@ def process_base(row) -> None:
         row["base"] = base_signal.id
 
 
-class SignalBaseResource(resources.ModelResource):
+class ModelResource(resources.ModelResource):
+    def get_field_names(self):
+        names = []
+        for field in self.get_fields():
+            names.append(self.get_field_name(field))
+        return names
+
+    def import_row(self, row, instance_loader, **kwargs):
+        # overriding import_row to ignore errors and skip rows that fail to import
+        # without failing the entire import
+        import_result = super(ModelResource, self).import_row(
+            row, instance_loader, **kwargs
+        )
+
+        if import_result.import_type in [RowResult.IMPORT_TYPE_ERROR, RowResult.IMPORT_TYPE_INVALID]:
+            import_result.diff = [row.get(name, "") for name in self.get_field_names()]
+
+            # Add a column with the error message
+            import_result.diff.append(
+                "Errors: {}".format([err.error for err in import_result.errors])
+            )
+            # clear errors and mark the record to skip
+            import_result.errors = []
+            import_result.import_type = RowResult.IMPORT_TYPE_SKIP
+
+        return import_result
+
+
+class SignalBaseResource(ModelResource):
     """
     Resource class for importing Signals base.
     """
@@ -180,7 +209,7 @@ class SignalBaseResource(resources.ModelResource):
         process_base(row)
 
 
-class SignalResource(resources.ModelResource):
+class SignalResource(ModelResource):
     """
     Resource class for importing and exporting Signal models
     """
